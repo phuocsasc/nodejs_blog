24ba7953700ab31d4cc74b2014f2754fed47350a
phuocsasc <phuoctran.22102004@gmail.com>
2025-07-18 11:38:12 +0700

    update basic

--- src/index.js
+++ src/index.js
@@ -9,8 +9,11 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/.well-known', express.static(path.join(__dirname, 'public/.well-known')));
 
+app.use(express.urlencoded());
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', engine({
@@ -23,11 +26,20 @@ app.set('views', path.join(__dirname, 'resources/views'));
 // Định nghĩa tuyến đường đi
 app.get('/', (req, res) => {
       res.render('home');
-})
+});
 app.get('/news', (req, res) => {
       res.render('news');
-})
+});
+
+app.get('/search', (req, res) => {
+      res.render('search');
+});
+
+app.post('/search', (req, res) => {
+      console.log(req.body)
+      res.send('');
+});
+
+
 // 127.0.0.1 - localhost
-app.listen(port, () => {
-      console.log(`App listening on port ${port} http://localhost:${port}/`);
-})
+app.listen(port, () => { console.log(`App listening on port ${port} http://localhost:${port}/`); });
