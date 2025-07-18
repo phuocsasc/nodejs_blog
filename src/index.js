a5cc28fc3b96c95bd4e9f3cfead3794f556a8394
phuocsasc <phuoctran.22102004@gmail.com>
2025-07-18 09:08:17 +0700

    remove css

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/.well-known', express.static(path.join(__dirname, 'public/.well-known')));
 
 // HTTP logger
 app.use(morgan('combined'));
