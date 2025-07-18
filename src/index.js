dc15f1fcbee5ce61e3a49ac908192a01f5256797
phuocsasc <phuoctran.22102004@gmail.com>
2025-07-18 23:13:46 +0700

    Refactor code: create routes, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes')
+      
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/.well-known', express.static(path.join(__dirname, 'public/.well-known')));
 
@@ -23,22 +25,8 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-// Định nghĩa tuyến đường đi
-app.get('/', (req, res) => {
-      res.render('home');
-});
-app.get('/news', (req, res) => {
-      res.render('news');
-});
-
-app.get('/search', (req, res) => {
-      res.render('search');
-});
-
-app.post('/search', (req, res) => {
-      console.log(req.body)
-      res.send('');
-});
+// Routes initial
+route(app);
 
 
 // 127.0.0.1 - localhost
---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const newRouter = require('./news')
+const siteRouter = require('./site')
+
+function route(app) {
+      // Định nghĩa tuyến đường đi
+      app.use('/news', newRouter)
+
+      app.use('/', siteRouter)
+
+
+}
+
+module.exports = route
